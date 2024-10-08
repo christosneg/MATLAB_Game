035cbed730b7cc51cfc9cbd8721942756c80b739
christos <neg.christos@gmail.com>
2024-10-08 12:11:28 +0300

    basic movement and gravity working properly

--- Game.m
+++ Game.m
@@ -1,10 +1,9 @@
 classdef Game < handle
     properties  
-        ground = 0;        
+        ground = @(x) 0 * x;        
         boardSize = [-1000 1000 -50 1000];
         time_previous = posixtime(datetime('now'));
-        player = Player();  
-        min_dt = 0.01; 
+        player = Player(); 
     end
     
     methods
@@ -14,23 +13,20 @@ classdef Game < handle
         
         function obj = updateData(obj)
 
+             %calculate dt
             current_time = posixtime(datetime('now'));
             dt = (current_time - obj.time_previous);
             obj.time_previous = current_time;
-            
-            dt = max(dt, obj.min_dt);
 
             obj.player.updatePlayerData(dt, obj.ground); 
         end
 
         function visualizeData(obj)
             cla;
-            hold on;
+            hold on;  
             
-            obj.player.visualizePlayer();
-            
-
-            plot([obj.boardSize(1), obj.boardSize(2)], [obj.ground, obj.ground], 'k-', 'LineWidth', 2);
+            obj.player.visualizePlayer();   %plot player
+            fplot(obj.ground,"LineStyle","-");
            
             hold off;
             axis(obj.boardSize);
